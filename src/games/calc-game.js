aae0ca5d40407306809d4454790b4fafe1b7b516
O. Marchenko <alex.mar4enko@gmail.com>
2021-11-16 15:54:02 -0800

    feat: calc game added

---
+++ src/games/calc-game.js
@@ -0,0 +1,43 @@
+import gameEngine from '../index.js';
+
+export default () => {
+  const rules = 'What is the result of the expression?';
+
+  const qaGenerator = () => {
+    const generateExpression = () => {
+      const getRandomInt = (min = 0, max = 99) => {
+        const minR = Math.ceil(min);
+        const maxR = Math.floor(max);
+        return Math.floor(Math.random() * (maxR - minR) + minR);
+      };
+      const getRandomOp = () => {
+        const operators = ['+', '-', '*'];
+        return operators[Math.floor(Math.random() * operators.length)];
+      };
+      return `${getRandomInt()} ${getRandomOp()} ${getRandomInt()}`;
+    };
+
+    const question = generateExpression();
+    console.log(question);
+
+    const solveExpression = (exp) => {
+      const [firstOperand, operator, secondOperand] = exp.split(' ');
+      switch (operator) {
+        case '+':
+          return (parseInt(firstOperand, 10) + parseInt(secondOperand, 10));
+        case '-':
+          return (parseInt(firstOperand, 10) - parseInt(secondOperand, 10));
+        case '*':
+          return (parseInt(firstOperand, 10) * parseInt(secondOperand, 10));
+        default:
+          return 'unknown operator';
+      }
+    };
+
+    const answer = solveExpression(question).toString();
+    console.log(answer);
+    return [question, answer];
+  };
+
+  gameEngine(rules, qaGenerator);
+};
