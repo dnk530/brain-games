cceb7495836a3e6bf8ba6a41fcfa4804e6a4cfc5
O. Marchenko <alex.mar4enko@gmail.com>
2021-11-17 15:30:59 -0800

    feat: gcd-game added

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import gcdGame from '../src/games/gcd-game.js';
+
+gcdGame();
---
+++ src/games/gcd-game.js
@@ -0,0 +1,28 @@
+import gameEngine from '../index.js';
+
+export default () => {
+  const rules = 'Find the greatest common divisor of given numbers.';
+
+  const generateRound = () => {
+    const getRandomInt = (min = 0, max = 99) => Math.floor(Math.random() * (max - min) + min);
+    const question = `${getRandomInt()} ${getRandomInt()}`;
+
+    const getAnswer = (str) => {
+      const [firstChunk, secondChunk] = str.split(' ');
+      const firstInt = parseInt(firstChunk, 10);
+      const secondInt = parseInt(secondChunk, 10);
+
+      const findGcd = (a, b) => {
+        console.log(a, b);
+        if (a === 0 || b === 0) return a + b;
+        if (a > b) return findGcd(a % b, b);
+        return findGcd(b % a, a);
+      };
+      return (findGcd(firstInt, secondInt));
+    };
+    const answer = getAnswer(question).toString();
+    return [question, answer];
+  };
+
+  gameEngine(rules, generateRound);
+};
