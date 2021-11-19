8a18c7bf7f46f9ebaea0e094bd870648cc771a64
O. Marchenko <alex.mar4enko@gmail.com>
2021-11-19 12:49:09 -0800

    refactor: brain-gcd

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -5,21 +5,19 @@ export default () => {
   const rules = 'Find the greatest common divisor of given numbers.';
 
   const generateRound = () => {
-    const question = `${getRandomInt()} ${getRandomInt()}`;
+    const firstInt = getRandomInt();
+    const secondInt = getRandomInt();
 
-    const getAnswer = (str) => {
-      const [firstChunk, secondChunk] = str.split(' ');
-      const firstInt = parseInt(firstChunk, 10);
-      const secondInt = parseInt(secondChunk, 10);
+    const question = `${firstInt} ${secondInt}`;
 
-      const findGcd = (a, b) => {
-        if (a === 0 || b === 0) return a + b;
-        if (a > b) return findGcd(a % b, b);
-        return findGcd(b % a, a);
-      };
-      return (findGcd(firstInt, secondInt));
+    const findGcd = (a, b) => {
+      if (a === 0 || b === 0) return a + b;
+      if (a > b) return findGcd(a % b, b);
+      return findGcd(b % a, a);
     };
-    const answer = getAnswer(question).toString();
+
+    const answer = findGcd(firstInt, secondInt).toString();
+
     return [question, answer];
   };
 
