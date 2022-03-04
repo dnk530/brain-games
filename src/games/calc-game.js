be64e04cb41ab4b48edecaf6b12b77827f132672
O. Marchenko <alex.mar4enko@gmail.com>
2022-03-04 12:07:05 -0800

    refactor: minor changes

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,35 +1,31 @@
 import gameEngine from '../index.js';
 import getRandomInt from '../common/random-int.js';
 
-export default () => {
-  const rulesDescription = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+const rulesDescription = 'What is the result of the expression?';
 
+const calculate = (firstOperand, secondOperand, operator) => {
+  switch (operator) {
+    case '+':
+      return firstOperand + secondOperand;
+    case '-':
+      return firstOperand - secondOperand;
+    case '*':
+      return firstOperand * secondOperand;
+    default:
+      throw new Error('unknown operator');
+  }
+};
+
+export default () => {
   const generateRound = () => {
     const firstOperand = getRandomInt();
     const secondOperand = getRandomInt();
-
-    const getRandomOperator = () => {
-      const operators = ['+', '-', '*'];
-      return operators[Math.floor(Math.random() * operators.length)];
-    };
-    const operator = getRandomOperator();
+    const operator = operators[getRandomInt(0, operators.length - 1)];
 
     const question = `${firstOperand} ${operator} ${secondOperand}`;
+    const answer = calculate(firstOperand, secondOperand, operator).toString();
 
-    const getAnswer = () => {
-      switch (operator) {
-        case '+':
-          return firstOperand + secondOperand;
-        case '-':
-          return firstOperand - secondOperand;
-        case '*':
-          return firstOperand * secondOperand;
-        default:
-          return 'unknown operator';
-      }
-    };
-
-    const answer = getAnswer().toString();
     return [question, answer];
   };
 
